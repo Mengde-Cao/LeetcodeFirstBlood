ea75040ae6cfd6c582b20051f0cfe5cc2e8af421
Wallace <caojiahao@geetest.com>
2019-03-25 17:07:29 +0800

    first blood

---
+++ 18-4_Sum.py
@@ -0,0 +1,50 @@
+"""
+给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+
+注意：
+
+答案中不可以包含重复的四元组。
+
+示例：
+
+给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+
+满足要求的四元组集合为：
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+"""
+
+
+# 同3sum，使用双指针，原先base的单层for循环换成双层
+def fourSum(nums: list[int], target: int) -> list[list[int]]:
+    result = []
+    if not nums or len(nums) < 4:
+        return result
+    nums.sort()
+    for i in range(len(nums) - 3):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        for j in range(i + 1, len(nums) - 2):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            base = nums[i] + nums[j]
+            left = j + 1
+            right = len(nums) - 1
+            while left < right:
+                sum = base + nums[left] + nums[right]
+                if sum > target:
+                    right -= 1
+                elif sum < target:
+                    left += 1
+                else:
+                    result.append([nums[i], nums[j], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+    return result
