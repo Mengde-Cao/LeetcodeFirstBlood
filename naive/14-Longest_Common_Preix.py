094465adda763b1fba2c723d4f366974ddad9d79
Wallace <caojiahao@geetest.com>
2020-08-24 15:56:42 +0800

    add 14

---
+++ naive/14-Longest_Common_Preix.py
@@ -0,0 +1,35 @@
+"""
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+
+通过次数341,360提交次数882,613
+"""
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ''
+        if len(strs) == 1:
+            return strs[0]
+        i = 1
+        for z in zip(*strs):
+            i += 1
+            if len(set(z)) > 1:
+                break
+        return strs[0][:i]
